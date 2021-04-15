912d06b911abdaf577c95b3d5697ee037bf0d51d
snidarian <cephalopod31956@gmail.com>
2021-04-15 13:51:37 -0400

    functioning argparse and db engine

--- manage_contacts.py
+++ manage_contacts.py
@@ -6,6 +6,8 @@ import sqlalchemy # to interact with database
 import texttable # to display information to screen
 import colorama # to color-highlight information for ease of use and clarity
 import os # since this is a terminal program, use of terminal commands will be required
+import argparse
+
 
 # color variable escape sequence definitions
 
@@ -40,15 +42,66 @@ def main_menu():
         
 
 
+# ---------------------------------------------------------------------------------
+# PROGRAM SETUP
+
+
+# Set up and parse database arguement
+
+parser = argparse.ArgumentParser(description="Manages contacts from encrypted persistent DB file")
+# program takes one arguemnt - filepath to database file to manage
+args = parser.add_argument("database", help="SQLite database file path", type=str)
+
+# prepare argument for use
+args = parser.parse_args()
 
 
 
+# Initiate connection with database 
 
-# Executions
+# create engine
+from sqlalchemy import create_engine
+engine = create_engine("sqlite:///{}".format(args.database), echo=False)
+
+
+# generate connection object - creates database in question if not already created
+connection_object = engine.connect()
+
+
+# Set up Metadata class
+from sqlalchemy import MetaData
+# alias for Metadataclass class
+Meta = MetaData()
+
+# set up primary Table 'contacts'
+from sqlalchemy import Table, Column, Integer, String
+
+# set up contacts table
+contacts = Table(
+    'contacts', Meta,
+    Column('id', Integer, primary_key = True),
+    Column('Firstname', String),
+    Column('Lastname', String),
+    Column("Sex", String),
+    Column('Phone #', String),
+    Column('Email', String),
+    Column('Address', String),
+    Column('Contact Description', String)
+)
+
+# Execute the creation of the Table 'contacts'
+Meta.create_all(engine)
+
+
+
+# -------------------------------------------------------------------------------------
+# EXECUTIONS
+
 
 if __name__=="__main__":
     print("mark0")
     main_menu()
+    print(args.database) # database
 
 
 
